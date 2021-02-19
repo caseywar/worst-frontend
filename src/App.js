93bac82a891bfbfc21e5fe0bb1de49844440ee69
Casey Warren <caseywar1017@gmail.com>
2021-02-19 17:34:10 -0500

    react fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import request from 'superagent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    albumData: [],
+    query: '',
+  }
+
+  componentDidMount = async () => {
+    await this.fetchAlbums();
+  }
+
+  fetchAlbums = async () => {
+    const data = await request.get(`https://cmw-lab06-backend-first.herokuapp.com/albums`);
+
+    this.setState({
+      albumData: data.body.results
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.albumData.map(album =>
+
+            <div>
+              <div>
+                <img src={album.image} alt="album" />
+              </div>
+              <p>{album.name}</p>
+              <p>{album.description}</p>
+              <p>{album.category}</p>
+              <p>{album.price}</p>
+            </div>)
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
